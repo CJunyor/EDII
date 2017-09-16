e28a3dd4134bab28c1741ed7f834cfb7cf2e9291
Cândido Júnior <c.junyor88@gmail.com>
2017-09-15 23:34:01 -0300

    Add files via upload

--- Struct Dinâmica 2/main.cpp
+++ Struct Dinâmica 2/main.cpp
@@ -27,6 +27,48 @@ int main()
     fn.write((char *)k.info, sizeof(int));
     fn.close();
     stc *STC=new stc;
-    Iniciar(STC);
+    //Iniciar(STC);
+    int SIZE=sizeof(MTD);
+    ifstream fp;
+    string ax;
+    cout("Digite o nome da Struct:");
+    cin>>ax;
+    fp.open(ax.c_str(), ifstream::binary);
+    if(!fp.is_open()){
+        cout("Nome Invalido");
+        return 0;
+    }
+    while(1){
+        fp.read((char *)&k.dado, SIZE);
+        if(fp.eof()) break;
+        cout(k.dado.tipo<<" "<<k.dado.nome<<";");
+        if(!Malocar(&k)){
+            cout("Nao foi possivel abrir a Struct");
+            fp.close();
+            return 0;
+        }
+        else if(k.dado.tipo=="int"){
+                fp.read((char *)k.info, sizeof(int));
+                STC->vars.push_back(k);
+        }
+        else if(k.dado.tipo=="char") {
+            fp.read((char *)k.info, sizeof(char));
+            STC->vars.push_back(k);
+        }
+        else if(k.dado.tipo=="float"){
+            fp.read((char *)k.info, sizeof(float));
+            STC->vars.push_back(k);
+        }
+        else if(k.dado.tipo=="double"){
+            fp.read((char *)k.info, sizeof(double));
+            STC->vars.push_back(k);
+        }
+        else if(k.dado.tipo=="string"){
+            fp.read((char *)k.info, sizeof(string));
+            STC->vars.push_back(k);
+        }
+    }
+    STC->nome=ax;
+    fp.close();
     return 0;
 }
